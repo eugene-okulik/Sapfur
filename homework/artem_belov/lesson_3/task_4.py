36790b31bbdb97a77112082b315ed96e00710600
Artem Belov <sapfur30@gmail.com>
2024-04-08 16:09:39 +0300

    fix for flake8 Lint

--- homework/artem_belov/lesson_3/task_4.py
+++ homework/artem_belov/lesson_3/task_4.py
@@ -1,7 +1,7 @@
 import math
 a = 4
 b = 5
-hepotenuse = math.sqrt(a**2 + b**2)
+hepotenuse = math.sqrt(a ** 2 + b ** 2)
 area = 0.5 * a * b
-print('Площадь треугольника:',area)
-print('Гипотенуза треугольника:',hepotenuse)
+print('Площадь треугольника:', area)
+print('Гипотенуза треугольника:', hepotenuse)
