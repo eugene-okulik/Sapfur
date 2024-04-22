d905ea774bd9db4bd4df6d2ec513ea0ec1827b41
Artem Belov <sapfur30@gmail.com>
2024-04-22 17:35:50 +0300

    fix lint

--- homework/artem_belov/lesson_9/task_2.py
+++ homework/artem_belov/lesson_9/task_2.py
@@ -3,7 +3,7 @@ import statistics
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
                 30, 28, 24, 23]
 
-hot_temperatures = list(filter(lambda x: x > 28,temperatures))
+hot_temperatures = list(filter(lambda x: x > 28, temperatures))
 
 print("Самая высокая температура:", max(hot_temperatures))
 print("Самая низкая температура:", min(hot_temperatures))
