970fcabc4543c649142862975378ab49d8b6e372
Artem Belov <sapfur30@gmail.com>
2024-11-15 16:28:05 +0300

    fix lint and delete file

--- homework/artem_belov/lesson_22/lesson_22.py
+++
@@ -1,9 +0,0 @@
-import requests
-from datetime import datetime
-
-
-start = datetime.now()
-response = requests.get('https://api.restful-api.dev/objects')
-print(response)
-end = datetime.now()
-print(end - start)
